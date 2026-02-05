e1f540e5eaea31bc81d11ad761cee817683f9b67
fallndoumbe <ndoumbefall163@gmail.com>
2026-02-05 18:09:25 +0000

    feat(todos): route

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show')
 Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+Route::get('/todos/{id}', [TodoController::class, 'show'])->name('todos.show');
